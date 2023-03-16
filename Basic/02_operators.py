cc6ae834a0288d6423a2e42c7f1ee9d82b662af0
DAvi777d <barrerajulian777@gmail.com>
2023-03-15 21:16:09 -0500

    aclaraciones en los comentarios

--- Basic/02_operators.py
+++ Basic/02_operators.py
@@ -28,8 +28,7 @@ un error
 '''
 my_float = 2.5 * 2               
 print("Hola " * int(my_float))
-my_float = 2.5 * 2
-print("Hola " * int(my_float))
+
 
 
 ### Operadores Comparativos ###
@@ -59,4 +58,4 @@ print(3 > 4 or "Hola" > "Python")
 print(3 < 4 and "Hola" < "Python")
 print(3 < 4 or "Hola" > "Python")
 print(3 < 4 or ("Hola" > "Python" and 4 == 4))
-print(not (3 > 4))
+print(not (3 > 4)) #convierte un falso en verdadero y lo verdadero en false 
